de67492e24d5068f03f029a94e1303642c520b97
unknown <Administrator@WAS-CHWU1-GIT.labs.microstrategy.com>
2015-01-27 20:31:54 +0000

    change helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub #41
+
+	// TODO Auto-generated method stub #41
 		System.out.println("Hello World!");
 	}
 
